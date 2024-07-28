d40ed631bb42fa849a1996c9ea027f28db0c776c
Dmitrii <idimka9@gmail.com>
2024-07-29 01:30:01 +0500

    add framer-motion animation

--- src/components/FilesystemItem.tsx
+++ src/components/FilesystemItem.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 import { ChevronRightIcon } from '@heroicons/react/16/solid';
 import { DocumentIcon, FolderIcon } from '@heroicons/react/24/solid';
 import { Node } from '../types';
@@ -9,32 +10,49 @@ function FilesystemItem({ node }: Readonly<{ node: Node }>) {
 
   return (
     <li key={node.name}>
-      <span className='flex items-center gap-1.5 py-1'>
+      <span className="flex items-center gap-1.5 py-1">
         {node.nodes && node.nodes.length > 0 && (
-          <button onClick={() => setIsOpen(!isOpen)} className='p-1 -m-1'>
-            <ChevronRightIcon className={`size-4 text-gray-500 ${isOpen ? 'rotate-90' : ''}`} />
+          <button onClick={() => setIsOpen(!isOpen)} className="p-1 -m-1">
+            <motion.span
+              animate={{ rotate: isOpen ? 90 : 0 }}
+              transition={{ type: 'spring', bounce: 0, duration: 0.4 }}
+              className="flex"
+            >
+              <ChevronRightIcon className="size-4 text-gray-500" />
+            </motion.span>
           </button>
         )}
 
         {node.nodes ? (
           <FolderIcon
-            className={`size-6 text-sky-500 ${node.nodes.length === 0 ? 'ml-[22px]' : ''}`}
+            className={`size-6 text-sky-500 ${
+              node.nodes.length === 0 ? 'ml-[22px]' : ''
+            }`}
           />
         ) : (
-          <DocumentIcon className='ml-[22px] size-6 text-gray-900' />
+          <DocumentIcon className="ml-[22px] size-6 text-gray-900" />
         )}
         {node.name}
       </span>
 
-      {isOpen && (
-        <ul className='pl-6'>
-          {node.nodes?.map((node) => (
-            <FilesystemItem node={node} key={node.name} />
-          ))}
-        </ul>
-      )}
+      <AnimatePresence>
+        {isOpen && (
+          <motion.ul
+            initial={{ height: 0 }}
+            animate={{ height: 'auto' }}
+            exit={{ height: 0 }}
+            transition={{ type: 'spring', bounce: 0, duration: 0.4 }}
+            className="pl-6 overflow-hidden flex flex-col justify-end"
+          >
+            {node.nodes?.map((node) => (
+              <FilesystemItem node={node} key={node.name} />
+            ))}
+          </motion.ul>
+        )}
+      </AnimatePresence>
     </li>
   );
 }
+
 
 export { FilesystemItem };
